be97465a896379ec344bc1acb1e9c14940cf5216
Allia <allia.jarjir@edu.devinci.fr>
2023-05-28 17:29:41 +0200

    d

--- javascript/deletebutton.js
+++ javascript/deletebutton.js
@@ -1,9 +1,7 @@
 const deletebtn = document.querySelectorAll(".deletebtn");
 
 deletebtn.forEach(function (icon) {
-    console.log(icon);
     icon.addEventListener("click", function () {
-        console.log("cc");
         window.confirm("Êtes-vous sûr de supprimer le post ?");
     });
 });
